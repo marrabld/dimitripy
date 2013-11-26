08011219a5fd09933fc2ad11dc7a5a7d14335921
Dan Marrable <marrabld@gmail.com>
2013-11-26 17:28:56 +0000

    LUT methods work for up and down T.  Need to check and make work for the other two types.

--- libdimitripy/lut_tool.py
+++ libdimitripy/lut_tool.py
@@ -35,7 +35,6 @@ class Lut():
 
         while not done:
 
-
             header_data = f.readline()
             if header_data[0] == '#':  # Then we have a comment
                 tmp_line = header_data.split(' ')
@@ -113,6 +112,10 @@ class Lut():
                     for col_num, col in enumerate(row):
                         rsr_2d[row_num - 1][col_num] = col
 
+            #rsr_2d = scipy.trim_zeros(rsr_2d)  # Some of the csv files have trailing lines
+            tmp_0 = scipy.trim_zeros(rsr_2d[:, 0])
+            tmp_1 = scipy.trim_zeros(rsr_2d[:, 1])
+            rsr_2d = scipy.vstack((tmp_0.T, tmp_1.T)).T
             rsr_list.append(rsr_2d)
 
         return rsr_list
@@ -125,45 +128,112 @@ class Lut():
         function_handle_list = []
 
         for rsr in rsr_list:
-            band_min.append(scipy.min(rsr[:, 0]))
-            band_max.append(scipy.max(rsr[:, 0]))
+            band_min.append(rsr[:, 0].min())
+            band_max.append(rsr[:, 0].max())
 
-        dimitri_lut_data = scipy.zeros(lut_labels['dimensions'])
+        ##########
+        #  The dimitri dimensions is the same as lut but with less waves
+        ##########
+        dimitri_lut_data_dim = lut_labels['dimensions']
+        dimitri_lut_data_dim[0] = len(rsr_list)
+        dimitri_lut_data = scipy.zeros(dimitri_lut_data_dim)
 
         if 'power_term' in lut_labels.keys():
             aerosol_label = 'power_term'
         elif 'tau550' in lut_labels.keys():
             aerosol_label = 'tau550'
 
-        for i_iter, power_term in enumerate(lut_labels[aerosol_label]):  # May be tau550 or powerterm
-            for j_iter, wind in enumerate(lut_labels['wind']):
-                for k_iter, delta_phi in enumerate(lut_labels['delta_phi']):
-                    for l_iter, theta_v in enumerate(lut_labels['theta_v']):
-                        for m_iter, theta_s in enumerate(lut_labels['theta_s']):
-                            # interpolate a function handle for the RSRs
-                            for i_iter, rsr_func in enumerate(rsr_list):
-                                ##########
-                                #  for all the RSRs we generate an interpolation function
-                                ##########
-                                f = scipy.interpolate.interp1d(rsr_list[i_iter][:, 0], rsr_list[i_iter][:, 1])
-                                function_handle_list.append(f)
+        if 'theta_s' in lut_labels:  # We must be a big LUT
+            for i_iter, power_term in enumerate(lut_labels[aerosol_label]):  # May be tau550 or powerterm
+                for j_iter, wind in enumerate(lut_labels['wind']):
+                    for k_iter, delta_phi in enumerate(lut_labels['delta_phi']):
+                        for l_iter, theta_v in enumerate(lut_labels['theta_v']):
+                            for m_iter, theta_s in enumerate(lut_labels['theta_s']):
+                                # interpolate a function handle for the RSRs
+                                for n_iter, rsr_func in enumerate(rsr_list):
+                                    ##########
+                                    #  for all the RSRs we generate an interpolation function
+                                    ##########
+                                    f = scipy.interpolate.interp1d(rsr_list[n_iter][:, 0], rsr_list[n_iter][:, 1])
 
-                            for n_iter, wavelength in enumerate(lut_labels['lambda']):
-                                #dimitri_lut_data[n_iter][m_iter][l_iter][k_iter][j_iter][i_iter] = 0
-                                pass  #  TODO all below.
-                                ##########
-                                #  For each wavelength, figure out which interpolation function to call
-                                #  find the wavelength in a band find the RSR weight
-                                #  The centre band is the [sum weight x LUT] / [sum weights]
-                                #  convolve the functions.
-                                ##########
+                                    ##########
+                                    #  Grab all the waves from the lut in our band
+                                    ##########
+                                    idx_wave_min = lut_labels['lambda'] >= band_min[n_iter]
+                                    idx_wave_max = lut_labels['lambda'] <= band_max[n_iter]
+                                    idx_wave = idx_wave_min & idx_wave_max
 
-                                ##########
-                                # Write the centre band value to the new LUT
-                                # write the new lut_label as well.
-                                ##########
-        pass
+                                    waves = lut_labels['lambda'][idx_wave]
+                                    lut_vals = lut[idx_wave, m_iter, l_iter, k_iter, j_iter, i_iter]
+                                    #if lut_vals.shape[0] != waves.shape[0]:
+                                    band_weights = 0
+                                    band_ref = 0
 
+                                    ##########
+                                    #  convolve the LUT and RSR
+                                    ##########
+                                    #! TEST
+                                    if 412 in waves and l_iter == 0:
+                                        print ('before convolution')
+                                        print(lut_vals)
+
+                                    for wave_iter, wave in enumerate(waves):
+                                        #lut_vals = lut[idx_wave][m_iter][l_iter][k_iter][j_iter][i_iter]
+                                        band_ref += f(wave) * lut_vals[wave_iter]
+                                        band_weights += f(wave)  # Sum all of the weights in the band
+
+                                    ##########
+                                    #  All of the waves are condensed down to one value which is the centre band
+                                    #  of the RSR
+                                    ##########
+
+                                    ##########
+                                    #  Need to figure out where to put the wavelength dim.  As they are unlinkely go be parsed in order
+                                    ##########
+                                    sorted_band_max = scipy.sort(band_max)
+                                    pos = scipy.linspace(0, len(band_max) ,len(band_max)+1)
+                                    wave_pos = int(pos[sorted_band_max == band_max[n_iter]])
+
+                                    if band_ref == 0 or band_weights == 0:
+                                        lg.warning('no data for this band :: ' + str(wave) + 'making -999')
+                                        dimitri_lut_data[wave_pos, m_iter, l_iter, k_iter, j_iter,
+                                                         i_iter] = -999  # for now
+                                    else:
+                                        dimitri_lut_data[wave_pos, m_iter, l_iter, k_iter, j_iter,
+                                                         i_iter] = band_ref / band_weights
+
+                                        #! TEST
+                                    if 412 in waves and l_iter == 0:
+                                        print ('after convolution')
+                                        print(dimitri_lut_data[n_iter, m_iter, l_iter, k_iter, j_iter,
+                                                               i_iter])
+        else:  # We are the small LUT
+            for i_iter, power_term in enumerate(lut_labels['tau550']):
+                for j_iter, rsr_func in enumerate(rsr_list):
+                    ##########
+                    #  for all the RSRs we generate an interpolation function
+                    ##########
+                    f = scipy.interpolate.interp1d(rsr_list[j_iter][:, 0], rsr_list[j_iter][:, 1])
+
+                    ##########
+                    #  Grab all the waves from the lut in our band
+                    ##########
+                    idx_wave = band_min[j_iter] <= lut_labels['lambda'] <= band_max[j_iter]
+                    waves = lut_labels['lambda'][idx_wave]
+                    band_weights = 0
+                    band_ref = 0
+
+                    for wave_iter, wave in enumerate(waves):
+                        band_ref += f(wave) * lut[wave_iter, j_iter]
+                        band_weights += f(wave)  # Sum all of the weights in the band
+
+                    ##########
+                    #  All of the waves are condensed down to one value which is the centre band
+                    #  of the RSR
+                    ##########
+                    dimitri_lut_data[i_iter, j_iter] = band_ref / band_weights
+
+        return dimitri_lut_data
 
 
     def file_len(self, fname):
@@ -176,6 +246,52 @@ class Lut():
 
         # These are indexes.
         return self.lut_data[power_term][wind][delta_phi][theta_v][theta_s][wavelength]
+
+
+    def write_trans_lut_to_file(self, lut, lut_labels, filename='lut.txt', header_txt='#'):
+
+        f = open(filename, 'wb')
+        writer = csv.writer(f, delimiter=' ')
+
+        ##########
+        #  Write header information
+        #  which we grab from lut_label
+        ##########
+
+        f.write(header_txt)
+        f.write('# lambda: ')
+        writer.writerow(lut_labels['lambda'])
+        f.write('# thetav: ')
+        writer.writerow(lut_labels['theta_v'])
+        f.write('# Inner loop is on theav, then on bands \n')
+        for wave_iter, wavelength in enumerate(lut_labels['lambda']):
+            for i_iter, theta_s in enumerate(lut_labels['theta_s']):
+                for j_iter, theta_v in enumerate(lut_labels['theta_v']):
+                    #f.write('theta_v :: ' + str(lut_labels['theta_v'][j_iter]) + ' ')
+                    for k_iter, delta_phi in enumerate(lut_labels['delta_phi']):
+                        #f.write('delta_phi :: ' + str(lut_labels['delta_phi'][k_iter]) + ' ')
+                        for l_iter, wind in enumerate(lut_labels['wind']):
+                            #for m_iter, tau550 in enumerate(lut_labels['tau550']):  # May be tau550 or power term
+                            val = lut[wave_iter, i_iter, j_iter, k_iter, l_iter, :]
+                            #val = lut[wave_iter, l_iter, k_iter, j_iter, i_iter, :]
+                            #val = lut[wave_iter, k_iter, l_iter, j_iter, i_iter, :]
+                            #val = lut[wave_iter, l_iter, j_iter, k_iter, i_iter, :]
+                            #val = lut[0, 0, 0, 0, 0, :]
+                            #print(theta_v)
+                            #f.write('lambda :: ' + str(lut_labels['lambda'][wave_iter]) + ' ')
+                            writer.writerow(val)
+
+
+        #
+        ##for i_iter, power_term in enumerate(self.labels['tau550']):  # May be tau550 or powerterm
+        #for l_iter, theta_v in enumerate(self.labels['theta_v']):
+        #    for m_iter, theta_s in enumerate(self.labels['theta_s']):
+        #        for j_iter, wind in enumerate(self.labels['wind']):
+        #            for k_iter, delta_phi in enumerate(self.labels['delta_phi']):
+        #                for n_iter, wavelength in enumerate(self.labels['lambda']):
+        #                    val = lut[n_iter][m_iter][l_iter][k_iter][j_iter][:]
+        #                    writer.writerow(val)
+
 
 
 
--- test/test_lut_tool.py
+++ test/test_lut_tool.py
@@ -1,6 +1,7 @@
 __author__ = 'marrabld'
 
 import sys
+
 sys.path.append("../..")
 
 import unittest
@@ -10,16 +11,23 @@ import libdimitripy.base
 import libdimitripy.brdf
 import libdimitripy.lut_tool
 import os
+import pylab
 
 
 class TestLutTool(unittest.TestCase):
     def setUp(self):
-        self.test_dir = '/home/marrabld/projects/dimitripy/scripts/data/LUTS/demo_mc50'
+        #self.test_dir = '/home/marrabld/projects/dimitripy/scripts/data/LUTS/demo_mc50'
+        self.test_dir = '/home/marrabld/projects/dimitripy/scripts/data/LUTS/hs386_mc50'
+
         self.rsr_dir = '/home/marrabld/projects/DIMITRI_2.0/AUX_DATA/spectral_response/MERIS'
-        test_file = 'demo_PP_V_MC50_BO_10000_func.txt'
-        test_file_2 = 'demo_tdown_PP_V_MC50_R010_100000_mean.txt'
-        test_file_3 = 'demo_tup_PP_V_MC50_R010_100000_mean.txt'
-        test_file_4 = 'demo_aot_PP_V_MC50.txt'
+        #test_file = 'demo_PP_V_MC50_BO_10000_func.txt'
+        #test_file_2 = 'demo_tdown_PP_V_MC50_R010_100000_mean.txt'
+        #test_file_3 = 'demo_tup_PP_V_MC50_R010_100000_mean.txt'
+        #test_file_4 = 'demo_aot_PP_V_MC50.txt'
+        test_file = 'hs386_PP_V_MC50_BO_10000_func.txt'
+        test_file_2 = 'hs386_tdown_PP_V_MC50_R010_100000_mean.txt'
+        test_file_3 = 'hs386_tup_PP_V_MC50_R010_100000_mean.txt'
+        test_file_4 = 'hs386_aot_PP_V_MC50.txt'
 
         self.test_file_1 = os.path.join(self.test_dir, test_file)
         self.test_file_2 = os.path.join(self.test_dir, test_file_2)
@@ -27,21 +35,52 @@ class TestLutTool(unittest.TestCase):
         self.test_file_4 = os.path.join(self.test_dir, test_file_4)
         self.lut = libdimitripy.lut_tool.Lut()
 
-    def test_read_lut(self):
-
-        lut = self.lut.read_lut(self.test_file_1)
-        self.assertIsInstance(lut, scipy.ndarray)
-
-        lut = self.lut.read_lut(self.test_file_2)
-        self.assertIsInstance(lut, scipy.ndarray)
-
-        lut = self.lut.read_lut(self.test_file_3)
-        self.assertIsInstance(lut, scipy.ndarray)
-
-        lut = self.lut.read_lut(self.test_file_4)
-        self.assertIsInstance(lut, scipy.ndarray)
-
-    def test_read_rsr_from_directory(self):
-
+    #def test_read_lut(self):
+    #    lut, lut_label = self.lut.read_lut(self.test_file_1)
+    #    self.assertIsInstance(lut, scipy.ndarray)
+    #
+    #    lut, lut_label = self.lut.read_lut(self.test_file_2)
+    #    self.assertIsInstance(lut, scipy.ndarray)
+    #
+    #    lut, lut_label = self.lut.read_lut(self.test_file_3)
+    #    self.assertIsInstance(lut, scipy.ndarray)
+    #
+    #    lut, lut_label = self.lut.read_lut(self.test_file_4)
+    #    self.assertIsInstance(lut, scipy.ndarray)
+    #
+    #def test_read_rsr_from_directory(self):
+    #    rsr_list = self.lut.read_rsr_from_directory(self.rsr_dir)
+    #    self.assertIsInstance(rsr_list, list)
+    #
+    def test_convolve_rsr_lut(self):
+        lut, lut_lables = self.lut.read_lut(self.test_file_3)
         rsr_list = self.lut.read_rsr_from_directory(self.rsr_dir)
-        self.assertIsInstance(rsr_list, list)
+        dimitri_lut = self.lut.convolve_rsr_lut(rsr_list, lut, lut_lables)
+        self.assertIsInstance(dimitri_lut, scipy.ndarray)
+        #pylab.plot(dimitri_lut[0, 0, 0, 0, 0, 0])
+        #pylab.show()
+#
+    def test_write_trans_lut_to_file(self):
+        lut, lut_lables = self.lut.read_lut(self.test_file_3)
+        print(lut.shape)
+        rsr_list = self.lut.read_rsr_from_directory(self.rsr_dir)
+        dimitri_lut = self.lut.convolve_rsr_lut(rsr_list, lut, lut_lables)
+        print(dimitri_lut.shape)
+        waves = scipy.asarray(
+            [412.5, 442.5, 490.0, 510.0, 560.0, 620.0, 665.0, 681.25, 708.75, 753.75, 761.875, 778.75, 865.0, 885.0,
+             900.0])
+        lut_lables['lambda'] = waves
+        header_string = "# MERIS total upward transmittance (direct+diffuse, Rayleigh+aerosol) for aerosol model MAR-50 \n\
+# Columns gives t_up for 7 aerosol optical thickness (total, i.e. all layers) given in file taua_1.txt \n\
+# (first optical thickness is zero hence gives Rayleigh transmittance) \n\
+# Lines are for all bands, thetas, thetav, deltaphi and wind speed given by:\n"
+
+        self.lut.write_trans_lut_to_file(dimitri_lut, lut_lables, 't_up_lut_test.txt', header_txt=header_string)
+
+
+    def test_lut_nums(self):
+        lut, lut_lables = self.lut.read_lut(self.test_file_3)
+        idx_wave = lut_lables['lambda'] == 412
+        print(lut[idx_wave, 0, 0, 0, 0, :])
+
+
